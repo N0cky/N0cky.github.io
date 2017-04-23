01c756c95164de360cb8ccfd2285aa89a99cadcd
Tobias Bunk <tobiasbunk19@googlemail.com>
2017-04-23 09:46:48 +0200

    Add files via upload

---
+++ AJAX.js
@@ -0,0 +1,37 @@
+function GetLEDState() {
+  nocache = "&nocache="\
+                  + Math.random() * 1000000;
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (this.readyState == 4) {
+      if (this.status == 200) {
+        if (this.responseText != null) {
+          document.getElementById("LED_txt")\
+          .innerHTML = this.responseText;
+        }
+      }
+    }
+  }
+  request.open("GET", "LED_State" + nocache, true);
+  request.send(null);
+  setTimeout('GetLEDState()', 1000);
+}
+
+function GetTemperature() {
+  nocache = "&nocache="\
+                  + Math.random() * 1000000;
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (this.readyState == 4) {
+      if (this.status == 200) {
+        if (this.responseText != null) {
+          document.getElementById("Temperature_txt")\
+          .innerHTML = this.responseText;
+        }
+      }
+    }
+  }
+  request.open("GET", "Temperature" + nocache, true);
+  request.send(null);
+  setTimeout('GetTemperature()', 1000);
+}
